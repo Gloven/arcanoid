3eb44783cac45e5cab7c81355b9156d4bf5c0294
Gloven <zzzskiffzzz@gmail.com>
2016-11-06 14:00:49 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Vlad on 25.10.2016.
+ */
+/*
+
+function draw(){
+    var canvas = document.getElementById('canvas'); // получить елемент холста
+    var context = canvas.getContext('2d');
+
+    function drawArc() {
+        context.fillStyle = ('grey');
+
+        context.beginPath();
+        context.strokeRect(50, 50, 10, 10);
+        context.fill();
+        context.rect((500/2-25/2),10,25,25);
+        context.stroke();
+    }
+
+
+drawArc();
+
+ }
+
+*/
+
+
+
