8a625c706d57207b1462c755aad50cfa0b078c8a
Gloven <zzzskiffzzz@gmail.com>
2016-11-06 14:25:56 +0200

    second commit

--- script.js
+++ script.js
@@ -3,24 +3,6 @@
  */
 /*
 
-function draw(){
-    var canvas = document.getElementById('canvas'); // получить елемент холста
-    var context = canvas.getContext('2d');
-
-    function drawArc() {
-        context.fillStyle = ('grey');
-
-        context.beginPath();
-        context.strokeRect(50, 50, 10, 10);
-        context.fill();
-        context.rect((500/2-25/2),10,25,25);
-        context.stroke();
-    }
-
-
-drawArc();
-
- }
 
 */
 
